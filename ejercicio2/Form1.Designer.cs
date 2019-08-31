7974633b64a807359427d0988ba38dc148d2d635
Kevin Arias <kevinlh1337@gmail.com>
2019-08-31 10:37:53 -0500

    Se agrega el ejercicio a la rama remota.

---
+++ ejercicio2/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.precio = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.total = new System.Windows.Forms.Label();
+            this.preciototal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(252, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ejercicio #2: Calcular el IVA.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.label2.Location = new System.Drawing.Point(195, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el precio del producto:";
+            // 
+            // precio
+            // 
+            this.precio.Location = new System.Drawing.Point(401, 43);
+            this.precio.Name = "precio";
+            this.precio.Size = new System.Drawing.Size(143, 20);
+            this.precio.TabIndex = 2;
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(421, 69);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(101, 24);
+            this.calcular.TabIndex = 3;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label3.Location = new System.Drawing.Point(360, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IVA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.label4.Location = new System.Drawing.Point(305, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Precio total:";
+            // 
+            // total
+            // 
+            this.total.AutoSize = true;
+            this.total.Location = new System.Drawing.Point(398, 146);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(13, 13);
+            this.total.TabIndex = 6;
+            this.total.Text = "0";
+            // 
+            // preciototal
+            // 
+            this.preciototal.AutoSize = true;
+            this.preciototal.Location = new System.Drawing.Point(398, 168);
+            this.preciototal.Name = "preciototal";
+            this.preciototal.Size = new System.Drawing.Size(13, 13);
+            this.preciototal.TabIndex = 7;
+            this.preciototal.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 191);
+            this.Controls.Add(this.preciototal);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.precio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox precio;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label total;
+        private System.Windows.Forms.Label preciototal;
+    }
+}
+
---
+++ ejercicio2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            int precio2 = int.Parse(precio.Text);
+
+            precio2 = precio2 * 19 / 100;
+
+            int total2 = precio2;
+
+            total.Text = total2.ToString();
+
+            int precio3 = int.Parse(precio.Text);
+            precio3 = precio3 + precio2;
+
+            preciototal.Text = precio3.ToString();
+        }
+    }
+}
